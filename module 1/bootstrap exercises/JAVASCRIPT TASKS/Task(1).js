4fef662563d88f7646bb7974636ff2c89daf9e37
reethu2005-all <reethureddypailla@gmail.com>
2025-05-27 21:28:20 +0530

    Delete module 1/bootstrap exercises directory

--- module 1/bootstrap exercises/JAVASCRIPT TASKS/Task(1).js
+++
@@ -1,7 +0,0 @@
-// Log welcome message to console
-console.log("Welcome to the Community Portal");
-
-// Alert when the page is fully loaded
-window.addEventListener('load', () => {
-    alert("Page is fully loaded. Welcome!");
-});
